d7ec844d305b3094d1b0da53748598d2dc9dc070
lyxiong0 <bear507@163.com>
2021-11-08 12:04:38 +0800

    修复复杂子查询主子表相关联

--- src/observer/sql/executor/execute_stage.cpp
+++ src/observer/sql/executor/execute_stage.cpp
@@ -770,7 +770,8 @@ RC ExecuteStage::do_select(const char *db, const Selects &selects, SessionEvent 
     {
       const Condition &right_attrcondition = sub_select->conditions[i];
       // 查看条件中是否存在与主查询相关的条件，关联子查询必有表名
-      if (condition.right_is_attr == 1 && condition.right_attr.relation_name != nullptr && strcmp(condition.right_attr.relation_name, selects.relations[0]) == 0) {
+      LOG_INFO("condition.right_is_attr = %d, condition.right_attr.relation_name = %s", condition.right_is_attr, condition.right_attr.relation_name);
+      if (condition.right_is_attr && condition.right_attr.relation_name != nullptr && strcmp(condition.right_attr.relation_name, selects.relations[0]) == 0) {
         // 加入子查询
         sub_select->relations[sub_select->relation_num++] = selects.relations[0];
         // 加上group by
@@ -779,7 +780,7 @@ RC ExecuteStage::do_select(const char *db, const Selects &selects, SessionEvent 
         is_related = true;
       }
 
-      if (condition.left_is_attr == 1 && condition.left_attr.relation_name != nullptr && strcmp(condition.left_attr.relation_name, selects.relations[0]) == 0) {
+      if (condition.left_is_attr && condition.left_attr.relation_name != nullptr && strcmp(condition.left_attr.relation_name, selects.relations[0]) == 0) {
         // 加入子查询
         sub_select->relations[sub_select->relation_num++] = selects.relations[0];
         // 加上group by
