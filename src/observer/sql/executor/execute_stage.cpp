31d740b5f40c78513d351419dedac74368c01fdb
lyxiong0 <bear507@163.com>
2021-11-08 14:06:21 +0800

    Revert "增加复杂子查询主子表相关联"
    This reverts commit d8342a59fee4f4bc527b0f59a755bc3ea5bb75a5.

--- src/observer/sql/executor/execute_stage.cpp
+++ src/observer/sql/executor/execute_stage.cpp
@@ -2001,7 +2001,7 @@ bool cmp_value(AttrType left_type, AttrType right_type, void *left_data, const s
     }
 
     float sub_res = left - right;
-    if (sub_res > -1e-1 && sub_res < 1e-1)
+    if (sub_res > -1e-3 && sub_res < 1e-3)
     {
       ans = 0;
     }
